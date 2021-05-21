dd27e9b663b4d5cd86fa195bb195d20009dc3c77
Oscar Miranda <oimirandaa08@gmail.com>
2021-05-21 02:50:58 -0500

    Complition of the brickbreaker game

--- src/Games/Main.java
+++ src/Games/Main.java
@@ -1,5 +1,21 @@
 package Games;
 
-public class Main {
-    
+import javax.swing.JFrame;
+
+public class Main 
+{
+	public static void main(String[] args) {
+		
+		JFrame obj = new JFrame();
+		
+		GamePlay gamePlay = new GamePlay();
+		obj.setBounds(10,10,700,600);
+		obj.setTitle("Breakout ball");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gamePlay);
+		
+		
+	}
 }
